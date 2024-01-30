6e4c3740269858a47e2d8ab02defbbcc68a85d88
Hemanth Thallapaneni <tallapaneni.hemanth@gmail.com>
2024-01-29 22:35:37 -0500

    Created env file.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyACM5F-HzNYNkw4FLWCRXIp1eqSRkqbgMM',
-    authDomain: 'hemanth-thallapaneni-portfolio.firebaseapp.com',
-    databaseURL:
-      'https://hemanth-thallapaneni-portfolio-default-rtdb.firebaseio.com',
-    projectId: 'hemanth-thallapaneni-portfolio',
-    storageBucket: 'hemanth-thallapaneni-portfolio.appspot.com',
-    messagingSenderId: '513793027749',
-    appId: '1:513793027749:web:3fb62048295f1f0f49a74c',
-    measurementId: 'G-22LK9DC197',
+    apiKey: import.meta.env['NG_APP_API_KEY'],
+    authDomain: import.meta.env['NG_APP_AUTH_DOMAIN'],
+    databaseURL: import.meta.env['NG_APP_DATABASE_URL'],
+    projectId: import.meta.env['NG_APP_PROJECT_ID'],
+    storageBucket: import.meta.env['NG_APP_STORAGE_BUCKET'],
+    messagingSenderId: import.meta.env['NG_APP_MESSAGING_SENDING_ID'],
+    appId: import.meta.env['NG_APP_APP_ID'],
+    measurementId: import.meta.env['NG_APP_MEASUREMENT_ID'],
   },
 };
